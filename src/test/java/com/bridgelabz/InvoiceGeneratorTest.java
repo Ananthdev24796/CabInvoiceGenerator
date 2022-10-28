adfa4a4b32f96c18d9b820247f3310a97baa1952
Ananthdev24796 <ananth.cr7@gmail.com>
2022-10-28 20:53:39 +0530

    Added:UC4

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -21,15 +21,17 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
+
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare(){
-        Ride[] rides = {new Ride(3.0,5), new Ride(0.2,1)};
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(3.0, 5), new Ride(0.2, 1)};
         double totalFare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(40,totalFare,0.0);
+        Assert.assertEquals(40, totalFare, 0.0);
     }
+
     @Test
-    public void givenRides_ShouldReturnRideDetails(){
-        Ride[] rides = {new Ride(3.0,5), new Ride(0.2,1)};
+    public void givenRides_ShouldReturnRideDetails() {
+        Ride[] rides = {new Ride(3.0, 5), new Ride(0.2, 1)};
         Object[] rideDetails = invoiceGenerator.getRideDetails(rides);
         int numOfRide = (int) rideDetails[0];
         double totalFare = (double) rideDetails[1];
@@ -37,5 +39,21 @@ public class InvoiceGeneratorTest {
         Assert.assertEquals(2, numOfRide, 0);
         Assert.assertEquals(40, totalFare, 0.0);
         Assert.assertEquals(20, avgFare, 0.0);
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnUserRideDetails() {
+        User[] users = {new User(1), new User(2)};
+        users[0].addRideToList(new Ride(3, 2));
+        users[0].addRideToList(new Ride(4, 5));
+        users[1].addRideToList(new Ride(3, 2));
+        int idToSearchFor = 1;
+        for (User user : users) {
+            Assert.assertEquals(idToSearchFor, user.getUserId());
+            if (user.getUserId() == idToSearchFor) {
+                System.out.println(user.getUserId() + "\n" + user.getRideList());
+            }
+            break;
+        }
     }
 }
